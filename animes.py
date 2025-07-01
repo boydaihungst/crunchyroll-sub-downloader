ba35ceb29a6853914f30304ab8f1b50a36270914
Huy Hoang <boydaihungst@gmail.com>
2025-07-01 22:33:05 +0700

    fix: season selection

--- animes.py
+++ animes.py
@@ -211,7 +211,7 @@ def click_load_more_btn(sb):
 def select_season_from_dropdown_list(sb, season):
     try:
         sb.wait_for_element_present("css selector", ".episode-list .erc-playable-collection", timeout=15)
-        if sb.is_element_present("css selector", ".season-info > h4 > span"):
+        if sb.is_element_present("css selector", ".season-info"):
             sb.click(".season-info", by="css selector")
             sb.wait_for_element_present("css selector", "[class^='dropdown-content__children']", timeout=15)
             sb.click(
